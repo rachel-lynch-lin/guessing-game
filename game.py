5c0bb9b0cc87d170c1ecd850909da169b9284952
Rachel Lynch <lynch.m.rachel@gmail.com>
2018-10-11 23:28:17 +0000

    Got first player choice

--- game.py
+++ game.py
@@ -1,13 +1,29 @@
 """A number-guessing game."""
 
+import random
+
 print("Welcome to the number guessing game!")
+
+x = 1
+y = 100
+
+number = random.randint(1, 100)
 
 def get_player_name():
     """Get player name"""
 
-    player_name = input("What is your name?\n>")
+    player_name = input("What is your name?\n> ")
 
     return player_name
 
-get_player_name()
+def choose_number():
+    """Give instructions to pick a number."""
 
+    print(f"{get_player_name()}, choose a number from {x} to {y}.")
+    player_choice = input("> ")
+
+    return player_choice
+
+choose_number()
+
+print(number)
