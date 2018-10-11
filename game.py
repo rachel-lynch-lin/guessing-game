dfd8b67e206f63f8a0f7bac1f712fbfd1cda8698
Rachel Lynch <lynch.m.rachel@gmail.com>
2018-10-11 23:18:30 +0000

    Get the player's name

--- game.py
+++ game.py
@@ -1,3 +1,13 @@
 """A number-guessing game."""
 
 print("Welcome to the number guessing game!")
+
+def get_player_name():
+    """Get player name"""
+
+    player_name = input("What is your name?\n>")
+
+    return player_name
+
+get_player_name()
+
